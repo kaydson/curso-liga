88dfc6a71532d099f54fd9d3c16e83ecae7664d0
kaydson <kaydson1988@gmail.com>
2021-08-28 19:46:35 -0300

    feat-app

---
+++ app/src/app/pages/main/main.page.ts
@@ -0,0 +1,18 @@
+/** Regiao dos imports */
+
+import { Component, OnInit } from '@angular/core';
+
+/** a classe que representa a pagina inicial */
+
+@Component({
+  selector: 'bird-main',
+  templateUrl: './main.page.html',
+  styleUrls: ['./main.page.scss'],
+})
+export class MainPage  {
+
+  constructor() { }
+
+ 
+
+}
